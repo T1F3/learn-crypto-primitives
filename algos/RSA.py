5b4b97ab8f24ea782c162e9e82f3af7dd32b97ac
T1F3 <tife1379@gmail.com>
2019-05-14 22:48:55 +0100

    flshed out encrypt, decrypt functions, next stop, string encoding

--- algos/RSA.py
+++ algos/RSA.py
@@ -1,68 +1,84 @@
 import random
 
 def findPrimes(lower,upper):
-	primArr = []
-	for num in range(lower,upper + 1):
-		if num > 1:
-			for i in range(2,num):
-				if (num % i) == 0:
-					break
-			else:
-				primArr.append(num)
-	return(primArr)
+        primArr = []
+        for num in range(lower,upper + 1):
+                if num > 1:
+                        for i in range(2,num):
+                                if (num % i) == 0:
+                                        break
+                        else:
+                                primArr.append(num)
+        return(primArr)
 
 primes = findPrimes(10,100)
 
 # def Euclid(a,b):
-# 	count = 0
-# 	carry = max(a,b)
-# 	div = min(a,b)
-# 	rem = carry%div
-# 	remArr = [carry,carry]
-# 	while(rem!=0):
-# 		carry = div
-# 		div = rem
-# 		rem = carry%rem
-# 		remArr[count%2] = rem
-# 		count+=1
-# 	return(max(remArr))
+#       count = 0
+#       carry = max(a,b)
+#       div = min(a,b)
+#       rem = carry%div
+#       remArr = [carry,carry]
+#       while(rem!=0):
+#               carry = div
+#               div = rem
+#               rem = carry%rem
+#               remArr[count%2] = rem
+#               count+=1
+#       return(max(remArr))
 
-def Euclid(a, b):  
-    if a == 0 : 
-        return b  
+def Euclid(a, b):
+    if a == 0 :
+        return b
     return Euclid(b%a, a)
 
 def genPubKey():
-	p = primes[random.randint(0,len(primes)-1)]
-	q = primes[random.randint(0,len(primes)-1)]
-	n = p*q
-	pq1 = (p-1)*(q-1)	
-	e = random.randint(2, pq1-1)
-	while( Euclid(e,pq1) !=1 ):
-		e = random.randint(2, pq1-1)
-	pubKey = (n,e)
-	return(p,q,pubKey)
+        p = primes[random.randint(0,len(primes)-1)]
+        q = primes[random.randint(0,len(primes)-1)]
+        n = p*q
+        pq1 = (p-1)*(q-1)
+        e = random.randint(2, pq1-1)
+        while( Euclid(e,pq1) !=1 ):
+                e = random.randint(2, pq1-1)
+        pubKey =  {'n':n, 'e':e} 
+        return(p,q,pubKey)
 
 
 def genPriKey(p,q,e,r):
-	n = p*q
-	pq1 = (p-1)*(q-1)
-	params = filter(lambda d:((d*e)%pq1)==1,range(1,r))
-	d = params[0]
-	return(n,d)
+        n = p*q
+        pq1 = (p-1)*(q-1)
+        params = filter(lambda d:((d*e)%pq1)==1,range(1,r))
+        d = params[0]
+        return( {'n':n, 'd':d} )
+
 
 params = genPubKey()
 
 
-PriKey = genPriKey(params[0],params[1],params[2][1],10000)
-PubKey = (params[2])
+rawPriKey = genPriKey(params[0],params[1],params[2]['e'],10000)
+rawPubKey = (params[2])
 
-print(PubKey,'--',PriKey)
+PriKey = 
+
+e = rawPubKey['e']
+d = rawPriKey['d']
+n = rawPriKey['n']
+
+
+text = 5
+
+print(text)
 
 def encrypt(text,e,n):
 	code = (text**e)%n
 	return code
 
+
+
+print(code)
+
 def decrypt(code,d,n):
 	text = (code**d)%n
 	return text
+
+print(decrypt(code,d,n))
